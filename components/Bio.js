24cedf87147fc3927c0ab18e6e1027589b36cf5e
626dlee <32500842+626dlee@users.noreply.github.com>
2019-03-06 15:10:06 -0800

    Organized components into folder
    Component changes:
    - Home page animation premature fade fix
    - About page team data changed (Position field added)
    - New products page addition (Product List)
    - Compilation of products (ProductList.js)

---
+++ components/Bio.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Button from "./Button";
+
+// function Bio(props) {
+//   return (
+//     <div className="bio-wrapper">
+//       <p>
+//         <img className="portrait" src={props.imgURL} />
+//         <h3 className="bios-name">{props.name}</h3>
+//         <p className="bios-desc">{props.description}</p>
+//       </p>
+//       <p>
+//         <Button name="Contact Me" />
+//       </p>
+//     </div>
+//   );
+// }
+
+class Bio extends React.Component {
+  render() {
+    return (
+      <div className="bio-wrapper">
+        <p>
+          <img className="portrait" src={this.props.imgURL} />
+          <h3 className="bios-name">{this.props.name}</h3>
+          <h4 className="bios-pos">{this.props.pos}</h4>
+          <p className="bios-desc">{this.props.description}</p>
+        </p>
+        <p>
+          <a href="/contact">
+            <Button name="Contact Me" />
+          </a>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Bio;
---
+++ components/Text.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+
+class Text extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div className="text-wrapper">
+        {/*<ScrollAnimation
+          animateIn="fadeInRight"
+          animateOut="fadeOut"
+          animatePreScroll="false"
+          offset="00"
+        >*/}
+        <h2 className="text-title">{this.props.title}</h2>
+        {/*</ScrollAnimation>*/}
+        {/*<ScrollAnimation
+          animateIn="fadeIn"
+          animateOut="fadeOut"
+          animatePreScroll="false"
+          offset="00"
+        >*/}
+        <a href={this.props.descLink}>
+          <button type="button" className="text-desc">
+            {this.props.desc}
+          </button>
+        </a>
+        {/* </ScrollAnimation>*/}
+        <br />
+      </div>
+    );
+  }
+}
+
+export default Text;
